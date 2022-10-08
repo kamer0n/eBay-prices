6336026852375c45b5c42f944fb8d1bf756a84f2
Kameron <2386181e@student.gla.ac.uk>
2022-10-08 01:26:48 +0100

    added a react front end, connected to express backend, able to add new coins from frontend interface

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 import express from 'express';
 import { getCoins, decomposeDB } from './firebase_functions.js';
 import { getPrices } from './priceScrape.js';
+import cors from 'cors';
 
 const app = express()
-const port = 3000
+const port = 9000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(cors());
 
-app.get('/', async function (req, res, next) {
+app.get('/api', async function (req, res, next) {
   //await getPrices('gibraltar 2012 one pound');
-
-  res.send(await getCoins());
+  console.log('ere'); 
+  res.json({coins :await getCoins()});
   decomposeDB();
 })
 
-app.get('/addCoin/', async function (req, res, next) {
-  res.send(await getPrices('gibraltar 2012 one pound'));
+
+app.get('/addCoin/:coin', async function (req, res, next) {
+  console.log('here');
+  console.log(req.params.coin);
+  if (req.params){
+  let search = req.params.coin;
+  await getPrices(search)
+  res.json({message : "success"});
   decomposeDB();
+  } else {
+    res.sendStatus(404);
+  }
 })
